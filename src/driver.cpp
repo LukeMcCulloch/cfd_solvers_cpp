f7a5518145cd4956583e5270b63834a567dc1b75
Luke McCulloch <tlukemcculloch@gmail.com>
2019-06-12 11:40:28 -0500

    Euler Unsteady 2D

--- src/driver.cpp
+++ src/driver.cpp
@@ -2,17 +2,17 @@
 //======================================
 // 1D Eiuler approximate Riemann sovler
 #include "../include/EulerShockTube1D.h"
-//#include "../include/EulerUnsteady2D.h"
+#include "../include/EulerUnsteady2D.h"
 
 //======================================
 // structured grids 2D class 
 #include "../include/gridGen2D.h"
 
-//using namespace EulerSolver1D; 
+
 
 int main(){
 
-    if (true){
+    if (false){
         EulerSolver1D::driverEuler1D();
     }else{
         driverGrid2D();
