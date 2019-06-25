fc3e9e161d2b4edd1cf906986f0e35890aaa3085
Luke McCulloch <tlukemcculloch@gmail.com>
2019-06-25 09:30:17 -0500

    numerical constants, grid types, parameters and arrays for solver, allocation, grid read-construct-check

---
+++ include/EulerUnsteady2D_basic_package.h
@@ -0,0 +1,51 @@
+//********************************************************************************
+//* Educationally-Designed Unstructured 2D (EDU2D) Code
+//*
+//*
+//*
+//*     This module belongs to the inviscid version: EDU2D-Euler-RK2
+//*
+//*
+//*
+//* This file contains 4 modules:
+//*
+//*  1. module edu2d_constants      - Some numerical values, e.g., zero, one, pi, etc.
+//*  2. module edu2d_grid_data_type - Grid data types: node, edge, face, element, etc.
+//*  3. module edu2d_my_main_data   - Parameters and arrays mainly used in a solver.
+//*  4. module edu2d_my_allocation  - Subroutines for dynamic allocation
+//*  5. module edu2d_grid_data      - Subroutines for reading/constructing/checking grid data
+//*
+//* All data in the modules can be accessed by the use statement, e.g., 'use constants'.
+//*
+//*
+//*
+//*        written by Dr. Katate Masatsuka (info[at]cfdbooks.com),
+//*
+//* the author of useful CFD books, "I do like CFD" (http://www.cfdbooks.com).
+//*
+//* This is Version 0 (July 2015).
+//* This F90 code is written and made available for an educational purpose.
+//* This file may be updated in future.
+//*
+//********************************************************************************
+
+//********************************************************************************
+//********************************************************************************
+//********************************************************************************
+//********************************************************************************
+//********************************************************************************
+//* 1. module edu2d_constants
+//*
+//* Some useful constants are defined here.
+//* They can be accessed by the use statement, 'use constants'.
+//*
+//*
+//*        written by Dr. Katate Masatsuka (info[at]cfdbooks.com),
+//*
+//* the author of useful CFD books, "I do like CFD" (http://www.cfdbooks.com).
+//*
+//* This is Version 0 (July 2015).
+//* This F90 code is written and made available for an educational purpose.
+//* This file may be updated in future.
+//*
+//********************************************************************************
