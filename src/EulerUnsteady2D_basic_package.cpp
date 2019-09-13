ae29bd54df474ae9dc7f3a6b114a9d8e091bb3f7
Luke McCulloch <tlukemcculloch@gmail.com>
2019-09-13 12:49:47 -0500

    reading in quad data

--- src/EulerUnsteady2D_basic_package.cpp
+++ src/EulerUnsteady2D_basic_package.cpp
@@ -339,8 +339,35 @@ void edu2d_my_main_data::MainData2D::read_grid(std::string datafile_grid_in,
     //     allocate( elm(ntria+i)%vtx(4))
     //     read(1,*) elm(ntria+i)%vtx(1), elm(ntria+i)%vtx(2), &
     //             elm(ntria+i)%vtx(3), elm(ntria+i)%vtx(4)
+
+    if (nquad > 0) {
+        for (size_t i = 1; i < nquad; i++) {
+            std::getline(infile, line);
+            std::istringstream in(line);
+            elm[i].nvtx = 4;
+            //allocate(elm(i)%vtx(3))
+            elm[i].vtx = new Array2D<int>(4,1) ;
+            //edu2d_grid_data_type::elm_type[i].vtx* = new Array2D<int>(3,1) ;
+            //edu2d_grid_data_type::elm_type*  elm = new edu2d_grid_data_type::elm_type[nelms];
+
+            std::string type;
+            in >> type;                  //and read the first whitespace-separated token
+
+
+            float x1,x2,x3,x4;
+            in >> x1 >> x2 >> x3 >> x4;       //now read the whitespace-separated floats
+            //read(1,*) elm(i)%vtx(1), elm(i)%vtx(2), elm(i)%vtx(3)
+            //iss >> elm[i].vtx[0] >> elm[i].vtx[1] >> elm[i].vtx[2];
+            elm[i].vtx[0] = x1;
+            elm[i].vtx[1] = x2;
+            elm[i].vtx[2] = x3;
+            elm[i].vtx[2] = x4;
+        }
+    }
     // end do
     // endif
+
+
 
     // //  Write out the grid data.
 
