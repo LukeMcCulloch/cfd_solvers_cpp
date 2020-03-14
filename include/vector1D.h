1156a27cd075ed295fc750e517fe7fc27fa7e04d
Luke McCulloch <tlukemcculloch@gmail.com>
2020-03-13 21:12:46 -0600

    switching incrementally to std containers for dynamically updated grid components

---
+++ include/vector1D.h
@@ -0,0 +1,55 @@
+//
+/ /Specialization of std::vector to CFD unstructured uses
+//
+//=================================
+#ifndef _VECTOR1D_
+#define _VECTOR1D_
+
+#include <vector>
+
+template <class T>
+class Vector1D{
+
+public:
+   // CONSTRUCTORS ----------------------------------------------------------
+   //Vector();                        // default init to zero
+   Vector1D( void );                        // initializes all components to zero
+   //Vector( T x, T y, T z); // initializes with specified components
+   Vector1D(const std::vector<T>& v );             // initializes from existing vector
+
+   // destructor
+   ~Vector1D ();
+
+   // append
+   push_back( T value);
+
+   // STORAGE ---------------------------------------------------------------
+   std::vector<T> array; // 1D dynamic array
+
+};
+
+
+// CONSTRUCTORS ----------------------------------------------------------------
+
+template <class T>
+Vector1D<T>::Vector1D( void ) {
+}
+
+template <class T>
+Vector1D<T>::Vector1D(const  std::vector<T>& v ) {
+   arrayy = v;
+}
+
+template<class T>
+Vector1D<T>::~Vector1D() {
+}
+
+// OPERATIONS ------------------------------------------------------------------
+
+template<class T>
+Vector1D<T>
+push_back(T value) {
+   array.push_back(value)
+}
+
+#endif
